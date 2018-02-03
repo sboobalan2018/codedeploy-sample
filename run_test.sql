0b001ba4807c519d7ccdbe86895bea2c96226509
sboobalan2018 <35377442+sboobalan2018@users.noreply.github.com>
2018-02-02 20:07:32 -0600

    Update run_test.sql

--- run_test.sql
+++ run_test.sql
@@ -1,4 +1,4 @@
-insert into aws_demo.test values (1,'BOOBALAN1');
-insert into aws_demo.test values (1,'BOOBALAN2');
-insert into aws_demo.test values (1,'BOOBALAN3');
+insert into aws_demo.test values ('BOOBALAN1');
+insert into aws_demo.test values ('BOOBALAN2');
+insert into aws_demo.test values ('BOOBALAN3');
 commit;
