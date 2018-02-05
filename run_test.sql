41a1518ee435d7fc2227eb83b3f5aca79b442835
sboobalan2018 <35377442+sboobalan2018@users.noreply.github.com>
2018-02-05 20:09:02 +0000

    Update run_test.sql

--- run_test.sql
+++ run_test.sql
@@ -1,4 +1,4 @@
-insert into test values (1,'TEST1');
-insert into test values (1,'TEST2');
-insert into test values (1,'TEST3');
+insert into aws_demo.test values (1,'TEST1');
+insert into aws_demo.test values (1,'TEST2');
+insert into aws_demo.test values (1,'TEST3');
 commit;
